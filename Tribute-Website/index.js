b8bde222ee4f5a7cfd300f4f02d34631c57407d1
Aqsa Nazir <aqsanazir39@gmail.com>
2024-07-29 20:14:38 +0500

    updated Files

---
+++ Tribute-Website/index.js
@@ -0,0 +1,23 @@
+// let modeBtn = document.querySelector("mode");
+// let currMode = "light-mode";
+
+// modeBtn = addEventListener("click" , ()=>{
+
+//   if(currMode === "light-mode"){
+//  currMode = "dark-mode";
+//  document.querySelector("body").style.backgroundColor = "black";
+//   }else{
+//     currMode = "light-mode";
+//     document.querySelector("body").style.backgroundColor = "white";
+//   }
+//  console.log(currMode);
+// })
+
+let x = prompt("Enter a number");
+if(x % 5 ===0){
+console.log("Congratulations!");
+}else{
+prompt("Try again");
+}
+
+
